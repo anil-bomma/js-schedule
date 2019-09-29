4d8d8664bda970205e44538af13f468911828f3d
bomma.anil <anil.bommaa@gmail.com>
2019-09-29 15:46:42 -0500

    issus fixed

--- main.js
+++ main.js
@@ -78,75 +78,71 @@ secTime = async (second) => {
 }
 
 const setBackTimer = async () => {
-    if (!interval) {
-        let m1 = document.querySelector("#m1").value;
-        let m2 = document.querySelector("#m2").value;
-        let s1 = document.querySelector("#s1").value;
-        let s2 = document.querySelector("#s2").value;
+    let m1 = document.querySelector("#m1").value;
+    let m2 = document.querySelector("#m2").value;
+    let s1 = document.querySelector("#s1").value;
+    let s2 = document.querySelector("#s2").value;
 
-        let totalMinute = parseInt(`${m1}${m2}`);
-        let totalSecond = parseInt(`${s1}${s2}`);
+    let totalMinute = parseInt(`${m1}${m2}`);
+    let totalSecond = parseInt(`${s1}${s2}`);
 
-        let oneMin = 60;
+    let oneMin = 60;
 
-        if (totalMinute) {
+    if (totalMinute) {
 
-            if (totalSecond) {
-                await secTime(totalSecond);
-            }
-
-            let timeOut = totalMinute * 60 * 1000;
-
-            --totalMinute;
-            let strTime = totalMinute.toString();
-            document.querySelector("#m1").value = strTime.length == 2 ? strTime.charAt(0) : "0";
-            document.querySelector("#m2").value = strTime.length == 2 ? strTime.charAt(1) : strTime;
-
-            oneMinInterval = setInterval(() => {
-                oneMin = 60;
-                --totalMinute;
-                if (totalMinute > 0) {
-                    let strTime = totalMinute.toString();
-                    document.querySelector("#m1").value = strTime.length == 2 ? strTime.charAt(0) : "0";
-                    document.querySelector("#m2").value = strTime.length == 2 ? strTime.charAt(1) : strTime;
-                }
-            }, 60000)
-
-
-            interval = setInterval(() => {
-                --oneMin;
-                let strTime = oneMin.toString();
-                document.querySelector("#s1").value = strTime.length == 2 ? strTime.charAt(0) : "0";
-                document.querySelector("#s2").value = strTime.length == 2 ? strTime.charAt(1) : strTime;
-            }, 1000)
-
-            setTimeout(() => {
-                interval = 0;
-                clearInterval(oneMinInterval);
-                clearInterval(interval);
-                document.querySelector("#s1").value = 0;
-                document.querySelector("#s2").value = 0;
-                window.alert("Timeout completed...!");
-            }, timeOut)
-
-        } else {
-            let timeOut = totalSecond * 1000;
-
-            interval = setInterval(() => {
-                --totalSecond;
-                let strTime = totalSecond.toString();
-                document.querySelector("#s1").value = strTime.length == 2 ? strTime.charAt(0) : "0";
-                document.querySelector("#s2").value = strTime.length == 2 ? strTime.charAt(1) : strTime;
-            }, 1000)
-
-            setTimeout(() => {
-                interval = 0;
-                clearInterval(interval);
-                document.querySelector("#s1").value = 0;
-                document.querySelector("#s2").value = 0;
-                window.alert("Timeout completed...!");
-            }, timeOut)
+        if (totalSecond) {
+            await secTime(totalSecond);
         }
+
+        let timeOut = totalMinute * 60 * 1000;
+
+        --totalMinute;
+        let strTime = totalMinute.toString();
+        document.querySelector("#m1").value = strTime.length == 2 ? strTime.charAt(0) : "0";
+        document.querySelector("#m2").value = strTime.length == 2 ? strTime.charAt(1) : strTime;
+
+        oneMinInterval = setInterval(() => {
+            oneMin = 60;
+            --totalMinute;
+            if (totalMinute > 0) {
+                let strTime = totalMinute.toString();
+                document.querySelector("#m1").value = strTime.length == 2 ? strTime.charAt(0) : "0";
+                document.querySelector("#m2").value = strTime.length == 2 ? strTime.charAt(1) : strTime;
+            }
+        }, 60000)
+
+
+        interval = setInterval(() => {
+            --oneMin;
+            let strTime = oneMin.toString();
+            document.querySelector("#s1").value = strTime.length == 2 ? strTime.charAt(0) : "0";
+            document.querySelector("#s2").value = strTime.length == 2 ? strTime.charAt(1) : strTime;
+        }, 1000)
+
+        setTimeout(() => {
+            clearInterval(oneMinInterval);
+            clearInterval(interval);
+            document.querySelector("#s1").value = 0;
+            document.querySelector("#s2").value = 0;
+            window.alert("Timeout completed...!");
+        }, timeOut)
+
+    } else {
+        let timeOut = totalSecond * 1000;
+
+        interval = setInterval(() => {
+            --totalSecond;
+            let strTime = totalSecond.toString();
+            document.querySelector("#s1").value = strTime.length == 2 ? strTime.charAt(0) : "0";
+            document.querySelector("#s2").value = strTime.length == 2 ? strTime.charAt(1) : strTime;
+        }, 1000)
+
+        setTimeout(() => {
+            clearInterval(interval);
+            document.querySelector("#s1").value = 0;
+            document.querySelector("#s2").value = 0;
+            window.alert("Timeout completed...!");
+        }, timeOut)
     }
 }
 
@@ -155,7 +151,6 @@ const stopBackTimer = () => {
 }
 
 const resetBackTimer = () => {
-    interval = 0;
     s1 = s2 = m1 = m2 = 0;
     clearInterval(interval);
     document.querySelector("#s1").value = 0;
